eee1b13c37d0c1e46e2e1d939a128adc7a77c499
Connor Link <connorlink248@gmail.com>
2022-08-28 22:24:34 -0500

    Deprecate Rust backend

---
+++ codegen_old/target/debug/deps/codegen-8e45740a5cddde04.d
@@ -0,0 +1,11 @@
+/Users/connor/Desktop/cpu-design/compiler/codegen/target/debug/deps/codegen-8e45740a5cddde04.rmeta: src/lib.rs src/bindings.rs src/driver.rs src/error.rs src/expression.rs src/statement.rs src/util.rs
+
+/Users/connor/Desktop/cpu-design/compiler/codegen/target/debug/deps/codegen-8e45740a5cddde04.d: src/lib.rs src/bindings.rs src/driver.rs src/error.rs src/expression.rs src/statement.rs src/util.rs
+
+src/lib.rs:
+src/bindings.rs:
+src/driver.rs:
+src/error.rs:
+src/expression.rs:
+src/statement.rs:
+src/util.rs:
